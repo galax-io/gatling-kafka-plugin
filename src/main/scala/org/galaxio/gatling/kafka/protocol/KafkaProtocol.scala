708333d363d6d6ea730301d7affe05605de436a0
red-bashmak <47944216+red-bashmak@users.noreply.github.com>
2025-05-02 20:09:28 +0200

    refactor: moved the sender and tracker pool to class fields to prevent their recreation when the protocol is built
    removed unnecessary interaction with futures

--- src/main/scala/org/galaxio/gatling/kafka/protocol/KafkaProtocol.scala
+++ src/main/scala/org/galaxio/gatling/kafka/protocol/KafkaProtocol.scala
@@ -8,6 +8,7 @@ import org.galaxio.gatling.kafka.protocol.KafkaProtocol.KafkaMatcher
 import org.galaxio.gatling.kafka.request.KafkaProtocolMessage
 
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
 import scala.concurrent.duration.FiniteDuration
 
@@ -34,6 +35,42 @@ object KafkaProtocol {
   }
 
   val kafkaProtocolKey: ProtocolKey[KafkaProtocol, KafkaComponents] = new ProtocolKey[KafkaProtocol, KafkaComponents] {
+
+//    private val blockingPool                                              = Executors.newCachedThreadPool()
+//    private val ec: ExecutionContextExecutorService                       = ExecutionContext.fromExecutorService(blockingPool)
+    private val senderRef: AtomicReference[KafkaSender]                   = new AtomicReference[KafkaSender]()
+    private val trackersPoolRef: AtomicReference[KafkaMessageTrackerPool] = new AtomicReference[KafkaMessageTrackerPool]()
+
+    private def getOrCreateSender(protocol: KafkaProtocol): KafkaSender = {
+      do {
+        val sender = senderRef.get()
+        if (sender != null) {
+          return sender
+        }
+      } while (!senderRef.compareAndSet(null, KafkaSender(protocol.producerProperties)))
+      senderRef.get()
+    }
+
+    private def getOrCreateTrackerPool(coreComponents: CoreComponents, protocol: KafkaProtocol): KafkaMessageTrackerPool = {
+      do {
+        val pool = trackersPoolRef.get()
+        if (pool != null) {
+          return pool
+        }
+      } while (
+        !trackersPoolRef.compareAndSet(
+          null,
+          KafkaMessageTrackerPool(
+            protocol.consumerProperties,
+            coreComponents.actorSystem,
+            coreComponents.statsEngine,
+            coreComponents.clock,
+          ),
+        )
+      )
+      trackersPoolRef.get()
+    }
+
     override def protocolClass: Class[Protocol] =
       classOf[KafkaProtocol].asInstanceOf[Class[Protocol]]
 
@@ -41,25 +78,19 @@ object KafkaProtocol {
       throw new IllegalStateException("Can't provide a default value for KafkaProtocol")
 
     override def newComponents(coreComponents: CoreComponents): KafkaProtocol => KafkaComponents =
-      kafkaProtocol => {
-        val blockingPool                        = Executors.newCachedThreadPool()
-        val ec: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(blockingPool)
-
-        val sender       = KafkaSender(kafkaProtocol.producerProperties)(ec)
-        val trackersPool = new KafkaMessageTrackerPool(
-          kafkaProtocol.consumerProperties,
-          coreComponents.actorSystem,
-          coreComponents.statsEngine,
-          coreComponents.clock,
+      kafkaProtocol =>
+        KafkaComponents(
+          coreComponents,
+          kafkaProtocol,
+          getOrCreateTrackerPool(coreComponents, kafkaProtocol),
+          getOrCreateSender(kafkaProtocol),
         )
-
-        KafkaComponents(coreComponents, kafkaProtocol, trackersPool, sender)
-      }
   }
 }
 
-case class KafkaProtocol(
-    producerTopic: String,
+final case class KafkaProtocol(
+//    @deprecated("use topics in requests builders", "1.0.0")
+    producerTopic: String, // TODO: remove after 1.1.0 (when topic moved from protocol to request builders)
     producerProperties: Map[String, AnyRef],
     consumerProperties: Map[String, AnyRef],
     timeout: FiniteDuration,
