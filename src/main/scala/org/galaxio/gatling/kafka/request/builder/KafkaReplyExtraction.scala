9371ac02d5af3dc90bed26aa3bf0254fd84a3799
John Axe <jigarkhwar88@gmail.com>
2026-05-18 21:30:27 -0300

    feat: align main with gatling 3.13

--- src/main/scala/org/galaxio/gatling/kafka/request/builder/KafkaReplyExtraction.scala
+++
@@ -1,8 +0,0 @@
-package org.galaxio.gatling.kafka.request.builder
-
-import org.galaxio.gatling.kafka.request.KafkaProtocolMessage
-
-case class KafkaReplyExtraction(
-    sessionKey: String,
-    extractor: KafkaProtocolMessage => Any,
-)
